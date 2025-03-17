17ef87a6fb2558b29b31b19000e53b05154a7024
Fardowza Ali <fardowza.a.2@gmail.com>
2025-03-17 13:27:09 -0500

    Constant update

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-let currentDate = new Date();
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let seconds = currentDate.getSeconds();
-console.log(hours);
-document.getElementById("hour").textContent = hours;
-document.getElementById("minute").textContent = minutes;
-document.getElementById("second").textContent = seconds;
+    function updateclock() {
+        let currentDate = new Date();
+        let hours = currentDate.getHours();
+        let minutes = currentDate.getMinutes();
+        let seconds = currentDate.getSeconds();
+        console.log(hours);
+        document.getElementById("hour").textContent = hours;
+        document.getElementById("minute").textContent = minutes;
+        document.getElementById("second").textContent = seconds;
+    }
+    setInterval(updateclock, 1000);
+    updateclock();
+
 });
